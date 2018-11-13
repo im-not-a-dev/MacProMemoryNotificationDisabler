a80ebf6aa0cb3a112cb2703fff92d65dba8667eb
Goldfish64 <goldfishnet@aol.com>
2018-11-12 18:49:39 -0600

    Cleanup; better patches

--- SystemProfilerMemoryFixup/kern_start.cpp
+++ SystemProfilerMemoryFixup/kern_start.cpp
@@ -22,13 +22,13 @@
 
 #include <Headers/plugin_start.hpp>
 #include <Headers/kern_api.hpp>
+#include <Headers/kern_file.hpp>
 
-// System Information binary path.
+// Pathes to binaries.
 static const char *binPathSystemInformation = "/Applications/Utilities/System Information.app/Contents/MacOS/System Information";
-
-// System Information libraries binary paths.
-static const char *binPathAppleSystemInfo = "/System/Library/PrivateFrameworks/AppleSystemInfo.framework/Versions/A/AppleSystemInfo";
 static const char *binPathSPMemoryReporter = "/System/Library/SystemProfiler/SPMemoryReporter.spreporter/Contents/MacOS/SPMemoryReporter";
+
+static const uint32_t SectionActive = 1;
 
 // MacBookAir name patches.
 static const uint8_t findAir[] = "MacBookAir";
@@ -41,17 +41,7 @@ static UserPatcher::BinaryModPatch patchAir {
     0,
     1,
     UserPatcher::FileSegment::SegmentTextCstring,
-    1
-};
-
-UserPatcher::BinaryModInfo ADDPR(binaryAirSystemInformation)[] {
-    { binPathSystemInformation, &patchAir, 1 }
-};
-UserPatcher::BinaryModInfo ADDPR(binaryAirSPMemoryReporter)[] {
-    { binPathSPMemoryReporter, &patchAir, 1 }
-};
-UserPatcher::BinaryModInfo ADDPR(binaryAirSystemInfo)[] {
-    { binPathAppleSystemInfo, &patchAir, 1 }
+    SectionActive
 };
 
 // MacBookPro10 name patches.
@@ -65,31 +55,110 @@ static UserPatcher::BinaryModPatch patchPro10 {
     0,
     1,
     UserPatcher::FileSegment::SegmentTextCstring,
-    1
+    SectionActive
 };
 
-UserPatcher::BinaryModInfo ADDPR(binaryPro10SystemInformation)[] {
-    { binPathSystemInformation, &patchPro10, 1 }
+// Find:    BF 02 00 00 00 E8 XX XX XX XX
+// Replace: B8 08 00 00 00 0F 1F 44 00 00
+static const size_t patchBytesCount = 10;
+static const uint8_t replaceBytes[patchBytesCount] = { 0xB8, 0x00, 0x00, 0x00, 0x00, 0x0F, 0x1F, 0x44, 0x00, 0x00 };
+
+// Patching info for System Information binary.
+static uint8_t findBytesSystemInformation[patchBytesCount] = { };
+static UserPatcher::BinaryModPatch patchBytesSystemInformation {
+    CPU_TYPE_X86_64,
+    findBytesSystemInformation,
+    replaceBytes,
+    patchBytesCount,
+    0,
+    1,
+    UserPatcher::FileSegment::SegmentTextText,
+    SectionActive
 };
-UserPatcher::BinaryModInfo ADDPR(binaryPro10SPMemoryReporter)[] {
-    { binPathSPMemoryReporter, &patchPro10, 1 }
+
+// Patching info for SPMemoryReporter binary.
+static uint8_t findBytesSPMemoryReporter[patchBytesCount] = { };
+static UserPatcher::BinaryModPatch patchBytesSPMemoryReporter {
+    CPU_TYPE_X86_64,
+    findBytesSPMemoryReporter,
+    replaceBytes,
+    patchBytesCount,
+    0,
+    1,
+    UserPatcher::FileSegment::SegmentTextText,
+    SectionActive
 };
-UserPatcher::BinaryModInfo ADDPR(binaryPro10SystemInfo)[] {
-    { binPathAppleSystemInfo, &patchPro10, 1 }
+
+// BinaryModInfo array containing all patches required.
+static UserPatcher::BinaryModInfo binaryPatches[] {
+    { binPathSystemInformation, &patchBytesSystemInformation, 1},
+    { binPathSPMemoryReporter, &patchBytesSPMemoryReporter, 1},
+    { binPathSystemInformation, &patchAir, 1 },
+    { binPathSPMemoryReporter, &patchAir, 1 }
+};
+
+// BinaryModInfo array containing all patches required for 10.8.
+// 10.8 does not have ASI_IsPlatformFeatureEnabled or strings in SPMemoryReporter.
+static UserPatcher::BinaryModInfo binaryPatchesML[] {
+    { binPathSystemInformation, &patchAir, 1 },
 };
 
 // System Information process info.
 static UserPatcher::ProcInfo procInfo = { binPathSystemInformation, static_cast<uint32_t>(strlen(binPathSystemInformation)), 1 };
 
+static void buildPatch(KernelPatcher &patcher, const char *path, uint8_t *findBuffer) {
+    DBGLOG("SystemProfilerMemoryFixup", "buildPatches() start");
+    
+    // Get contents of binary.
+    size_t outSize;
+    uint8_t *buffer = FileIO::readFileToBuffer(path, outSize);
+    if (buffer == NULL)
+        panic("SystemProfilerMemoryFixup: Failed to read binary: %s\n", path);
+    
+    // Find where ASI_IsPlatformFeatureEnabled is called.
+    off_t index = 0;
+    for (off_t i = 0; i < outSize; i++) {
+        if (buffer[i] == 0xBF && buffer[i+1] == 0x02 && buffer[i+2] == 0x00
+            && buffer[i+3] == 0x00 && buffer[i+4] == 0x00 && buffer[i+5] == 0xE8) {
+            index = i;
+            break;
+        }
+    }
+    
+    // If we found no match, we can't go on.
+    if (index == 0)
+        panic("SystemProfilerMemoryFixup: Failed to get index into binary: %s\n", path);
+    
+    // Build find pattern.
+    uint8_t *bufferOffset = buffer + index;
+    for (uint32_t i = 0; i < patchBytesCount; i++)
+        findBuffer[i] = bufferOffset[i];
+    
+    // Free buffer.
+    Buffer::deleter(buffer);
+}
+
+static void buildPatches(void *user, KernelPatcher &patcher) {
+    // Build patches for binaries.
+    buildPatch(patcher, binPathSystemInformation, findBytesSystemInformation);
+    buildPatch(patcher, binPathSPMemoryReporter, findBytesSPMemoryReporter);
+}
+
 // Main function.
 static void spmemfxStart() {
-    // Do product name patches.
-    lilu.onProcLoadForce(&procInfo, 1, nullptr, nullptr, ADDPR(binaryAirSystemInformation), 1);
-    lilu.onProcLoadForce(&procInfo, 1, nullptr, nullptr, ADDPR(binaryAirSystemInfo), 1);
-    lilu.onProcLoadForce(&procInfo, 1, nullptr, nullptr, ADDPR(binaryAirSPMemoryReporter), 1);
-    lilu.onProcLoadForce(&procInfo, 1, nullptr, nullptr, ADDPR(binaryPro10SystemInformation), 1);
-    lilu.onProcLoadForce(&procInfo, 1, nullptr, nullptr, ADDPR(binaryPro10SystemInfo), 1);
-    lilu.onProcLoadForce(&procInfo, 1, nullptr, nullptr, ADDPR(binaryPro10SPMemoryReporter), 1);
+    DBGLOG("SystemProfilerMemoryFixup", "start");
+    
+    // Are we on 10.9 or above?
+    if (getKernelVersion() >= KernelVersion::Mavericks) {
+        // Load callback so we can determine patterns to search for.
+        lilu.onPatcherLoad(buildPatches);
+        
+        // Load patches into Lilu.
+        lilu.onProcLoadForce(&procInfo, 1, nullptr, nullptr, binaryPatches, arrsize(binaryPatches));
+    } else if (getKernelVersion() == KernelVersion::MountainLion) {
+        // 10.8 requires only a single patch.
+        lilu.onProcLoadForce(&procInfo, 1, nullptr, nullptr, binaryPatchesML, arrsize(binaryPatchesML));
+    }
 }
 
 // Boot args.
@@ -103,6 +172,7 @@ static const char *bootargBeta[] {
     "-spmemfxbeta"
 };
 
+// Plugin configuration.
 PluginConfiguration ADDPR(config) {
     xStringify(PRODUCT_NAME),
     parseModuleVersion(xStringify(MODULE_VERSION)),
